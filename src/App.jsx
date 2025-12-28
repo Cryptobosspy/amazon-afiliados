c651322eb57219d100de738927cc167fb3aff39a
cryptobosspy <cryptobosspy@gmail.com>
2025-12-28 11:02:59 -0300

    Render language switcher in header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import Home from "./pages/Home"
 import Footer from "./components/Footer"
+import LanguageSwitcher from "./components/LanguageSwitcher"
 
 export default function App() {
   return (
     <>
+      {/* HEADER */}
+      <header className="max-w-6xl mx-auto px-4 py-4 flex justify-end">
+        <LanguageSwitcher />
+      </header>
+
       <Home />
       <Footer />
     </>
